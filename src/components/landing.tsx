8ff89b78b0ca5cff58852235ce1cf916907ee9de
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-09-12 16:45:27 -0400

    woooo

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -27,28 +27,28 @@ export function LandingPage(){
     return(
         <div className="flex h-screen w-full flex-col  items-center justify-center bg-gradient-to-tl from-darkcharcol from-10% to-slategreen to-75%">
           <NavBar/>
-          <div className="container flex flex-col-reverse items-center lg:flex-row p-5">
+          <div className="container flex flex-col-reverse items-center p-5 md:flex-row ">
             <div className=" text-white">
-            <div className="flex flex-row justify-center items-end">
-              <p className="font-Montserrat text-8xl font-extrabold italic">D3</p>
-              <p className="text-6xl font-Montserrat font-semibold italic pr-5 pb-1">VELOP</p>
-            </div>
-              <p className="mt-2 text-md font-Poppins">
-              Crafting the Future of Film Development
-              </p>
-              <p className="mt-2 text-xs font-Poppins">
-              Advanced Tools for Film Creators, Built by Experts, Inspired by Tradition.
-              </p>
-              <div className="mt-8">
-                <a
-                  href="#"
-                  className="inline-block px-4 py-2 text-lg font-medium text-white bg-steelblue  rounded-lg hover:bg-blue-700"
-                >
-                  Get Started
-                </a>
+              <div className="flex flex-row justify-center items-end">
+                <p className="font-Montserrat text-8xl font-extrabold italic">D3</p>
+                <p className="text-6xl font-Montserrat font-semibold italic pr-5 pb-1">VELOP</p>
+              </div>
+                <p className="mt-2 text-md lg:text-lg font-Poppins">
+                Crafting the Future of Film Development
+                </p>
+                <p className="mt-2 text-xs lg:text-md font-Poppins">
+                Advanced Tools for Film Creators, Built by Experts, Inspired by Tradition.
+                </p>
+                <div className="mt-8">
+                  <a
+                    href="#"
+                    className="inline-block px-4 py-2 text-lg font-medium text-white bg-steelblue  rounded-lg hover:bg-blue-700"
+                  >
+                    Get Started
+                  </a>
               </div>
             </div>
-            <div className=" flex w-full justify-center">
+            <div className=" flex w-auto justify-center p-5">
               <div className=" w-full h-auto rounded-lg overflow-auto">
               <img  
                   src={images[currentImageIndex]}
