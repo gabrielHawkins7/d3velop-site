bbd320fb13aaef4490ac0ed26245130d67634eae
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-09-12 15:54:37 -0400

    I hate vercell

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
 
 export function NavBar(){
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    
 
   return (
     <nav className="fixed left-0 top-0 w-full flex justify-end text-white ">
