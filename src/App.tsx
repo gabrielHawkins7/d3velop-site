7127aefe851ca7ed78b47e216aa83be5431f7a5b
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-09-12 13:02:37 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { NavBar } from './components/navbar'
 import { LandingPage } from './components/landing'
 import { Showcase } from './components/showcase'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
