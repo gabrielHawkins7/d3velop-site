14b12f81ff4fb0865908d4355069d89a7d3eb761
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-09-15 18:16:54 -0400

    stuff

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,6 +17,10 @@ export function NavBar(){
   return (
     <nav className="fixed left-0 top-0 w-full text-white p-4 md:p-1 bg-shadowgray ">
       <div className="hidden md:flex flex-row w-full justify-end items-center pr-4">
+      <div className="flex flex-row w-full items-end">
+          <p className="font-Montserrat text-3xl font-extrabold italic pl-2 pr-[4px]">D3</p>
+          <p className="font-Montserrat text-xl font-extrabold italic">VELOP</p>
+        </div>
         <a href="#" className="hover:text-steelblue block p-4">About</a>
         <a
           className="inline-block px-4 py-2 text-md font-Poppins text-white border-2 rounded-lg hover:bg-DeepTeal">
