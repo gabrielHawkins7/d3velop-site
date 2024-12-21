338bf32aa20a07fff23288a63f53fd5743d90408
gabrielHawkins7 <hawkinsglh@outlook.com>
2024-12-21 17:50:01 -0500

    New Look

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App(){
+  return (
+    <>
+      <video muted={true} loop={true} autoPlay={true} src={'/Background_01-2.webm'} width="100%" height="auto" >
+      </video>
+    
+    </>
+  )
+}
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Home.tsx'
-import './index.css'
-import "@fontsource/montserrat"; // Defaults to weight 400
-import "@fontsource/poppins"; // Defaults to weight 400
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Home.tsx';
-import About from './About.tsx';
-
-
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
